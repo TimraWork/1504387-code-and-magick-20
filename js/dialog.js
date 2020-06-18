1b98e70b5a24bd92e02be2ca2dda1171970e08ca
Мухамеджанова Эльмира <timra.work@gmail.com>
2020-06-18 11:27:30 +0500

    Учебный проект: в движении

---
+++ js/dialog.js
@@ -0,0 +1,114 @@
+'use strict';
+(function () {
+  var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+  var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+  var wizardFireBall = document.querySelector('.setup-fireball-wrap');
+
+  var onCoatColorChange = function () {
+    window.util.colorize(wizardCoat, 'coat');
+  };
+
+  var onEyesColorChange = function () {
+    window.util.colorize(wizardEyes, 'eyes');
+  };
+
+  var onFireBallColorChange = function () {
+    window.util.colorize(wizardFireBall, 'fireball');
+  };
+
+  var userNameInput = document.querySelector('.setup-user-name');
+  var userNameMinLength = userNameInput.getAttribute('minlength');
+  var userNameMaxLength = userNameInput.getAttribute('maxlength');
+
+  var onNameInputInvalid = function () {
+    if (userNameInput.validity.tooShort) {
+      userNameInput.setCustomValidity('имя персонажа не может содержать менее ' + userNameMinLength + ' символов');
+    } else if (userNameInput.validity.tooLong) {
+      userNameInput.setCustomValidity('максимальная длина имени персонажа — ' + userNameMaxLength + ' символов');
+    } else if (userNameInput.validity.valueMissing) {
+      userNameInput.setCustomValidity('обязательное поле');
+    } else {
+      userNameInput.setCustomValidity('');
+    }
+  };
+  var onNameInputChange = function () {
+    var valueLength = userNameInput.value.length;
+    if (valueLength > userNameMinLength || valueLength < userNameMaxLength) {
+      userNameInput.setCustomValidity('');
+    }
+  };
+
+  var userDialog = document.querySelector('.setup');
+  var userDialogBtnOpen = document.querySelector('.setup-open .setup-open-icon');
+  var userDialogBtnClose = document.querySelector('.setup-close');
+
+  userDialogBtnOpen.addEventListener('click', function () {
+    openUserDialog();
+  });
+
+  userDialogBtnOpen.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter') {
+      openUserDialog();
+    }
+  });
+
+  var onUserDialogEscPress = function (evt) {
+    if (evt.key === 'Escape' && !evt.target.matches('.setup-user-name')) {
+      closeUserDialog();
+    }
+  };
+
+  var onUserDialogEnterPress = function (evt) {
+    if (evt.key === 'Enter') {
+      closeUserDialog();
+    }
+  };
+
+  var onUserDialogClose = function () {
+    closeUserDialog();
+  };
+
+  var initUserDialogEventHandlers = function () {
+    document.addEventListener('keydown', onUserDialogEscPress);
+
+    userDialogBtnClose.addEventListener('keydown', onUserDialogEnterPress);
+    userDialogBtnClose.addEventListener('click', onUserDialogClose);
+
+    userNameInput.addEventListener('invalid', onNameInputInvalid);
+    userNameInput.addEventListener('input', onNameInputChange);
+
+    wizardCoat.addEventListener('click', onCoatColorChange);
+    wizardEyes.addEventListener('click', onEyesColorChange);
+    wizardFireBall.addEventListener('click', onFireBallColorChange);
+
+    window.dragAndDrop.init();
+  };
+
+  var destroyUserDialogEventHandlers = function () {
+    document.removeEventListener('keydown', onUserDialogEscPress);
+
+    userDialogBtnClose.removeEventListener('keydown', onUserDialogEnterPress);
+    userDialogBtnClose.removeEventListener('click', onUserDialogClose);
+
+    userNameInput.removeEventListener('invalid', onNameInputInvalid);
+    userNameInput.removeEventListener('input', onNameInputChange);
+
+    wizardCoat.removeEventListener('click', onCoatColorChange);
+    wizardEyes.removeEventListener('click', onEyesColorChange);
+    wizardFireBall.removeEventListener('click', onFireBallColorChange);
+
+    window.dragAndDrop.destroy();
+  };
+
+  var openUserDialog = function () {
+    userDialog.classList.remove('hidden');
+    userDialog.removeAttribute('style');
+    initUserDialogEventHandlers();
+  };
+
+  var closeUserDialog = function () {
+    userDialog.classList.add('hidden');
+    destroyUserDialogEventHandlers();
+  };
+
+})();
